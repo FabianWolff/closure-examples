45ecdefe30cc8e97a67f016c2c93de4a1e31e9ef
Aurel Bílý <aurel.bily@gmail.com>
2021-04-14 11:45:47 +0200

    verified version of result_uoe

--- result_uoe.rs
+++ result_uoe.rs
@@ -1,19 +1,67 @@
-// requires: match r { Ok(_) => true,
-//     Err(x) => f |=! |arg| { requires: arg == outer(x) } }
-// ensures: match old(r) {
-//     Ok(x) => result == x
-//     Err(x) => old(f) (x) ~~> { outer(result) == result }
-// }
-fn unwrap_or_else<T, E, F: FnOnce (E) -> T>
-       (r: Result<T, E>, f: F) -> T {
-    match r {
-        Ok(x) => x,
-        Err(x) => f(x)
+use prusti_contracts::*;
+
+// Prusti glue for Result<i32, i32>
+    enum MyResult {
+        Ok(i32),
+        Err(i32),
     }
-}
+
+    impl MyResult {
+        #[pure]
+        fn is_ok(&self) -> bool {
+            match self {
+                MyResult::Ok(_) => true,
+                MyResult::Err(_) => false,
+            }
+        }
+
+        #[pure]
+        #[requires(self.is_ok())]
+        fn unwrap_ok(&self) -> i32 {
+            match self {
+                MyResult::Ok(val) => *val,
+                MyResult::Err(_) => unreachable!(),
+            }
+        }
+
+        #[pure]
+        #[requires(!self.is_ok())]
+        fn unwrap_err(&self) -> i32 {
+            match self {
+                MyResult::Ok(_) => unreachable!(),
+                MyResult::Err(err) => *err,
+            }
+        }
+
+        #[requires(
+            !self.is_ok() ==>
+                f |=! |arg: i32| [ requires(arg == self.unwrap_err()) ]
+        )]
+        #[ensures(
+            old(self.is_ok()) ==> result == old(self.unwrap_ok())
+        )]
+        #[ensures(
+            !old(self.is_ok()) ==>
+                f ~>! |arg: i32|
+                    { arg == old(self.unwrap_err()) }
+                    { cl_result == result }
+        )]
+        fn unwrap_or_else<F: FnMut(i32) -> i32>(self, f: &mut F) -> i32 {
+            match self {
+                MyResult::Ok(x) => x,
+                MyResult::Err(x) => f(x),
+            }
+        }
+    }
+// end Prusti glue
 
 fn main() {
-    let err = Err(42);
-    assert_eq! ((42, 42),
-        unwrap_or_else(err, |x| (x, x)));
+    let mut cl = closure!(
+        #[requires(err < 0 && err > -10000)]
+        #[ensures(result == -err)]
+        |err: i32| -> i32 { -err }
+    );
+
+    assert!(MyResult::Ok(7).unwrap_or_else(&mut cl) == 7);
+    assert!(MyResult::Err(-42).unwrap_or_else(&mut cl) == 42);
 }
