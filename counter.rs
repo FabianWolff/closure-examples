f44ead22b6ae8b9548c7a61dbeb2e3aff0862581
Aurel Bílý <aurel.bily@gmail.com>
2021-04-14 23:49:11 +0200

    verified version of counter

--- counter.rs
+++ counter.rs
@@ -1,18 +1,25 @@
+use prusti_contracts::*;
+
 // From Kassios and Müller (2010) (simplified)
 
-#[requires(f |= || { ensures(result >= 1) })]
-fn foo(f: impl FnMut() -> i32) {}
+#[requires(f |= || [ requires(true), ensures(cl_result >= 2) ])]
+fn foo<T: FnMut() -> i32>(f: T) {}
 
 fn main() {
     let mut x = 0;
-    let mut inc =
-        #[invariant(x >= 0)]
-        #[invariant(old(x) <= x)]
-        #[ensures(old(x) + 1 == x)]
-        || { let r = x; x += 1; r };
+    let mut inc = closure!(
+        #[view(x: i32)]
+        #[invariant(*views.x >= 0)]
+        #[invariant(old(*views.x) <= *views.x)]
+        #[requires(true)]
+        #[ensures(old(*views.x) + 1 == *views.x)]
+        #[ensures(result == old(*views.x))]
+        || -> i32 { let r = x; x += 1; r }
+    );
 
-    // foo(inc); // fails to verify
+    inc();
+    //foo(inc); // fails to verify
     inc();
     foo(inc);
-    assert!(x >= 1);
+    //assert!(x >= 2);
 }
--- counter_err.rs
+++ counter_err.rs
@@ -1,15 +1,22 @@
+use prusti_contracts::*;
+
 // From Kassios and Müller (2010) (simplified)
 
-#[requires(f |= || { ensures(result >= 1) })]
-fn foo(f: impl FnMut() -> i32) {}
+#[requires(f |= || [ requires(true), ensures(cl_result >= 2) ])]
+fn foo<T: FnMut() -> i32>(f: T) {}
 
 fn main() {
     let mut x = 0;
-    let mut inc =
-        #[invariant(x >= 0)]
-        #[invariant(old(x) <= x)]
-        #[ensures(old(x) + 1 == x)]
-        || { let r = x; x += 1; r };
+    let mut inc = closure!(
+        #[view(x: i32)]
+        #[invariant(*views.x >= 0)]
+        #[invariant(old(*views.x) <= *views.x)]
+        #[requires(true)]
+        #[ensures(old(*views.x) + 1 == *views.x)]
+        #[ensures(result == old(*views.x))]
+        || -> i32 { let r = x; x += 1; r }
+    );
 
-    foo(inc); // <-- precondition (foo's) violation here
+    inc();
+    foo(inc); //~ ERROR precondition might not hold
 }
